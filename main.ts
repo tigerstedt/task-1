87a7b734868c88f39c8814c3460e76b2eb0360ca
Katinka Tigerstedt <70886896+tigerstedt@users.noreply.github.com>
2021-08-25 14:43:39 +0200

    Startfiler for MakeCode-prosjektet

---
+++ main.ts
@@ -0,0 +1,19 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . # . # .
+        # # # # #
+        # # # # #
+        . # # # .
+        . . # . .
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        # . . . #
+        . # . # .
+        . . # . .
+        . # . # .
+        # . . . #
+        `)
+})
+basic.showIcon(IconNames.Happy)
